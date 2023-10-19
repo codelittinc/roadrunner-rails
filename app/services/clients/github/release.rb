95b27baf102870e45ab74e7bf03de0bdaa170971
Kaio Magalhaes <me@kaiomagalhaes.com>
2023-10-19 15:56:53 -0300

    load all releases from github

--- app/services/clients/github/release.rb
+++ app/services/clients/github/release.rb
@@ -4,9 +4,19 @@ module Clients
   module Github
     class Release < GithubBase
       def list(repository)
-        releases = @client.list_releases(repository.full_name, {
-                                           per_page: 100
-                                         })
+        current_page = 0
+        per_page = 100
+        releases = []
+        max_not_reached = true
+
+        while max_not_reached
+          current_page += 1
+          new_releases = list_releases(repository, per_page, current_page)
+          max_not_reached = new_releases.size == per_page
+          releases = [releases, new_releases].flatten
+          releases = releases.sort_by(&:created_at)
+        end
+
         releases.map do |release|
           Clients::Github::Parsers::ReleaseParser.new(release)
         end
@@ -24,6 +34,13 @@ module Clients
       # @TODO: review this method, it does not seem to work
       def delete(url)
         @client.delete_release(url)
+      end
+
+      def list_releases(repository, per_page, page_number)
+        @client.list_releases(repository.full_name, {
+                                           per_page: per_page,
+                                           page: page_number
+                                         })
       end
     end
   end
--- app/services/flows/sub_flows/release_candidate_flow.rb
+++ app/services/flows/sub_flows/release_candidate_flow.rb
@@ -3,7 +3,7 @@
 module Flows
   module SubFlows
     class ReleaseCandidateFlow < BaseReleaseSubFlow
-      DEFAULT_TAG_NAME = 'rc.1.v0.0.0'
+      DEFAULT_TAG_NAME = "rc.1.v0.0.0"
       RELEASE_REGEX = /v(\d+)\.(\d+)\.(\d+)/
       RELEASE_CANDIDATE_VERSION_REGEX = /^rc\.(\d+)\./
 
@@ -35,15 +35,15 @@ module Flows
         return @source_control_release_commits if @source_control_release_commits
 
         @source_control_release_commits = if @releases.empty?
-                                            source_control_client.list_branch_commits(base_branch).reverse
-                                          else
-                                            source_control_client.compare_commits(version_resolver.latest_tag_name,
-                                                                                  base_branch)
-                                          end
+            source_control_client.list_branch_commits(base_branch).reverse
+          else
+            source_control_client.compare_commits(version_resolver.latest_tag_name,
+                                                  base_branch)
+          end
       end
 
       def version_resolver
-        @version_resolver ||= Versioning::ReleaseVersionResolver.new(environment, tag_names, 'update')
+        @version_resolver ||= Versioning::ReleaseVersionResolver.new(environment, tag_names, "update")
       end
     end
   end
